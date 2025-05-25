26edce5c8679f9b324a8b853cf266b1cebfd2ea6
Igor Leite <LeiteigorMatheus4@gmail.com>
2025-05-25 13:20:37 -0300

    Update TicketViewSet to restrict HTTP methods to GET, POST, and PATCH

--- ticket/viewsets.py
+++ ticket/viewsets.py
@@ -7,6 +7,7 @@ from rest_framework.filters import OrderingFilter
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    http_method_names = ['get', 'post', 'patch']
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['user', 'origin', 'destination', 'navigation_type']
     ordering_fields = ['purchase_date', 'travel_date']
